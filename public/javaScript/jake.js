7c23cc79d8f5ffcb3445e04c762fb2e955d9bac5
Jake Marsh <JMarsh2201@gmail.com>
2018-09-20 15:46:42 -0600

    minor update

--- public/javaScript/jake.js
+++ public/javaScript/jake.js
@@ -3,10 +3,12 @@ window.addEventListener('keydown', function(e) {
   // back ticks allow the curly dollar (template string).
   // e.keycode returns keycode of pressed key
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   // basically if audio = null, stop running the function
   if(!audio) return;
   // allows audio files to not need to finish before playing the next input
   audio.currentTime = 0;
   // plays the sound!
-  audio.play()
+  audio.play();
+  console.log(key);
 });
