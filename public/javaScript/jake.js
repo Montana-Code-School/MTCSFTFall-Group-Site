f70c9bae0657d6a7b6193716a0e2af770b69a99c
Jake Marsh <JMarsh2201@gmail.com>
2018-09-19 23:16:21 -0600

    Drum Machine init

--- public/javaScript/jake.js
+++ public/javaScript/jake.js
@@ -1,9 +1,12 @@
-console.log("ye gotta loicense for that script, mate?")
-
-var elem = document.querySelector('button');
-
-let btnThng = function(tag) {
-  return tag.style.margin = 30px
-}
-
-document.getElementsByTagName('button').addEventListener('click', btnThng(elem))
+window.addEventListener('keydown', function(e) {
+  // selects an 'audio' element. [] selects data type in said element
+  // back ticks allow the curly dollar (template string).
+  // e.keycode returns keycode of pressed key
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  // basically if audio = null, stop running the function
+  if(!audio) return;
+  // allows audio files to not need to finish before playing the next input
+  audio.currentTime = 0;
+  // plays the sound!
+  audio.play()
+});
